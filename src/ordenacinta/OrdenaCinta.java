54df0cf3fb16258318566a1bdbd96fc98d8762aa
OoDEGPoO <Oo.DEGP.oO@gmail.com>
2019-01-04 19:55:52 +0100

    Versión final (o casi)
    Arreglo de errores, y añadido de otros métodos de ordenación para la busqueda por algoritmo Voraz

--- src/ordenacinta/OrdenaCinta.java
+++ src/ordenacinta/OrdenaCinta.java
@@ -19,9 +19,10 @@ public class OrdenaCinta {
     
     //Funciones Auxiliares para la ejecución
     
-    public static void imprimeCinta(int[] id, int[] l, int[] p){
+    public static void imprimeCinta(int[] id, int[] l, int[] p, String titulo){
         int i;
-        
+        System.out.println("Tiempo de lectura: " + funcionTiempo(l,p));
+        System.out.println(titulo);
         System.out.println("-----------------------------------------------------------------");
         System.out.print("-\t");
         for (i = 0; i < id.length; i++){
@@ -43,7 +44,6 @@ public class OrdenaCinta {
         }
         System.out.println("-");
         System.out.println("-----------------------------------------------------------------");
-
     }
     
     private static int[][] generaCinta(int n, int minl, int maxl, int minp, int maxp){
@@ -67,9 +67,176 @@ public class OrdenaCinta {
         return out;
     }
     
-    //función ordenadora
+    // Para obtener el tiempo total de lectura de la cinta con la ordenación dada
+    private static int funcionTiempo(int[] l, int[] p){
+        int out = 0;
+        int i, j, s;
+        
+        for (i = 0; i < p.length; i++){
+            s = 0;
+            
+            for (j = 0; j <= i; j++){
+                s = s + l[j];
+            }
+            
+            out = out + (p[i] * s);
+        }
+        
+        return out;
+    }
     
-    private static int[][] ordenaCinta(int[] id, int[] l, int[] p){
+    //-----         funciones ordenadoras           -----
+    
+    // Por longitud de los ficheros (Creciente)
+    private static int[][] ordenaCintaLongitudCrec(int[] id, int[] l, int[] p){
+        int[][] out = {new int[id.length], new int[l.length], new int[p.length]};
+        int i, j;
+        
+        int aux;
+        boolean ord = false;
+        
+        //ordenamos por burbuja respecto al array de longitudes l
+        i = 0;
+        while ((i < id.length) && (!ord)){
+            i = i+1;
+            ord = true;
+            
+            for (j = 0; j < (id.length-i); j++){
+                if(l[j] > l[j+1]){//------------------ > para menor a mayor
+                    ord = false;
+                    aux = id[j];
+                    id[j] = id[j+1];
+                    id[j+1] = aux;
+                    
+                    aux = l[j];
+                    l[j] = l[j+1];
+                    l[j+1] = aux;
+                    
+                    aux = p[j];
+                    p[j] = p[j+1];
+                    p[j+1] = aux;
+                }
+            }
+        }
+        
+        out[0] = id; out[1] = l; out[2] = p;
+        
+        return out;
+    }
+    
+    // Por longitud de los ficheros (Decreciente)
+    private static int[][] ordenaCintaLongitudDecr(int[] id, int[] l, int[] p){
+        int[][] out = {new int[id.length], new int[l.length], new int[p.length]};
+        int i, j;
+        
+        int aux;
+        boolean ord = false;
+        
+        //ordenamos por burbuja respecto al array de longitudes l
+        i = 0;
+        while ((i < id.length) && (!ord)){
+            i = i+1;
+            ord = true;
+            
+            for (j = 0; j < (id.length-i); j++){
+                if(l[j] < l[j+1]){//------------------ < para mayor a menor
+                    ord = false;
+                    aux = id[j];
+                    id[j] = id[j+1];
+                    id[j+1] = aux;
+                    
+                    aux = l[j];
+                    l[j] = l[j+1];
+                    l[j+1] = aux;
+                    
+                    aux = p[j];
+                    p[j] = p[j+1];
+                    p[j+1] = aux;
+                }
+            }
+        }
+        
+        out[0] = id; out[1] = l; out[2] = p;
+        
+        return out;
+    }
+    
+    // Por nº de peticiones de los ficheros (Decreciente)
+    private static int[][] ordenaCintaPeticionesDecr(int[] id, int[] l, int[] p){
+        int[][] out = {new int[id.length], new int[l.length], new int[p.length]};
+        int i, j;
+        
+        int aux;
+        boolean ord = false;
+        
+        //ordenamos por burbuja respecto al array de longitudes l
+        i = 0;
+        while ((i < id.length) && (!ord)){
+            i = i+1;
+            ord = true;
+            
+            for (j = 0; j < (id.length-i); j++){
+                if(p[j] < p[j+1]){//------------------ < para mayor a menor
+                    ord = false;
+                    aux = id[j];
+                    id[j] = id[j+1];
+                    id[j+1] = aux;
+                    
+                    aux = l[j];
+                    l[j] = l[j+1];
+                    l[j+1] = aux;
+                    
+                    aux = p[j];
+                    p[j] = p[j+1];
+                    p[j+1] = aux;
+                }
+            }
+        }
+        
+        out[0] = id; out[1] = l; out[2] = p;
+        
+        return out;
+    }
+    
+    // Por nº de peticiones de los ficheros (Creciente)
+    private static int[][] ordenaCintaPeticionesCrec(int[] id, int[] l, int[] p){
+        int[][] out = {new int[id.length], new int[l.length], new int[p.length]};
+        int i, j;
+        
+        int aux;
+        boolean ord = false;
+        
+        //ordenamos por burbuja respecto al array de longitudes l
+        i = 0;
+        while ((i < id.length) && (!ord)){
+            i = i+1;
+            ord = true;
+            
+            for (j = 0; j < (id.length-i); j++){
+                if(p[j] > p[j+1]){//------------------ > para menor a mayor
+                    ord = false;
+                    aux = id[j];
+                    id[j] = id[j+1];
+                    id[j+1] = aux;
+                    
+                    aux = l[j];
+                    l[j] = l[j+1];
+                    l[j+1] = aux;
+                    
+                    aux = p[j];
+                    p[j] = p[j+1];
+                    p[j+1] = aux;
+                }
+            }
+        }
+        
+        out[0] = id; out[1] = l; out[2] = p;
+        
+        return out;
+    }
+    
+    // Por el producto del nº de peticiones y la longitud de los ficheros (Creciente)
+    private static int[][] ordenaCintaProductoCrec(int[] id, int[] l, int[] p){
         int[][] out = {new int[id.length], new int[l.length], new int[p.length]};
         int[] m = new int[id.length];
         int i, j;
@@ -77,37 +244,168 @@ public class OrdenaCinta {
         //obtenemos el producto para la ordenacion respecto a él
         for (i = 0; i<m.length; i++)    m[i] = l[i]*p[i];
         
-        int[] outId = new int[id.length];
-        int[] outL = new int[l.length];
-        int[] outP = new int[p.length];
-        int[] outM = new int[m.length];
-        int auxId, auxL, auxP, auxM;
+        int aux;
         boolean ord = false;
         
         //ordenamos por burbuja respecto al array de productos m
-        i = 1;
+        i = 0;
         while ((i < id.length) && (!ord)){
             i = i+1;
             ord = true;
             
             for (j = 0; j < (id.length-i); j++){
-                if(m[j] > m[j+1]){
+                if(m[j] > m[j+1]){//------------------ > para menor a mayor
                     ord = false;
-                    auxId = id[j];
+                    aux = id[j];
                     id[j] = id[j+1];
-                    id[j+1] = auxId;
+                    id[j+1] = aux;
                     
-                    auxL = l[j];
+                    aux = l[j];
                     l[j] = l[j+1];
-                    l[j+1] = auxL;
+                    l[j+1] = aux;
                     
-                    auxP = p[j];
+                    aux = p[j];
                     p[j] = p[j+1];
-                    p[j+1] = auxP;
+                    p[j+1] = aux;
                     
-                    auxM = m[j];
+                    aux = m[j];
                     m[j] = m[j+1];
-                    m[j+1] = auxM;
+                    m[j+1] = aux;
+                }
+            }
+        }
+        
+        out[0] = id; out[1] = l; out[2] = p;
+        
+        return out;
+    }
+    
+    // Por el producto del nº de peticiones y la longitud de los ficheros (Decreciente)
+    private static int[][] ordenaCintaProductoDecr(int[] id, int[] l, int[] p){
+        int[][] out = {new int[id.length], new int[l.length], new int[p.length]};
+        int[] m = new int[id.length];
+        int i, j;
+        
+        //obtenemos el producto para la ordenacion respecto a él
+        for (i = 0; i<m.length; i++)    m[i] = l[i]*p[i];
+        
+        int aux;
+        boolean ord = false;
+        
+        //ordenamos por burbuja respecto al array de productos m
+        i = 0;
+        while ((i < id.length) && (!ord)){
+            i = i+1;
+            ord = true;
+            
+            for (j = 0; j < (id.length-i); j++){
+                if(m[j] < m[j+1]){//------------------ < para mayor a menor
+                    ord = false;
+                    aux = id[j];
+                    id[j] = id[j+1];
+                    id[j+1] = aux;
+                    
+                    aux = l[j];
+                    l[j] = l[j+1];
+                    l[j+1] = aux;
+                    
+                    aux = p[j];
+                    p[j] = p[j+1];
+                    p[j+1] = aux;
+                    
+                    aux = m[j];
+                    m[j] = m[j+1];
+                    m[j+1] = aux;
+                }
+            }
+        }
+        
+        out[0] = id; out[1] = l; out[2] = p;
+        
+        return out;
+    }
+    
+    // Por la división del nº de peticiones y la longitud de los ficheros (Creciente)
+    private static int[][] ordenaCintaDivisionCrec(int[] id, int[] l, int[] p){
+        int[][] out = {new int[id.length], new int[l.length], new int[p.length]};
+        int[] c = new int[id.length];
+        int i, j;
+        
+        //obtenemos el producto para la ordenacion respecto a él
+        for (i = 0; i<c.length; i++)    c[i] = p[i]/l[i];
+        
+        int aux;
+        boolean ord = false;
+        
+        //ordenamos por burbuja respecto al array de cocientes c
+        i = 0;
+        while ((i < id.length) && (!ord)){
+            i = i+1;
+            ord = true;
+            
+            for (j = 0; j < (id.length-i); j++){
+                if(c[j] > c[j+1]){//------------------ > para menor a mayor
+                    ord = false;
+                    aux = id[j];
+                    id[j] = id[j+1];
+                    id[j+1] = aux;
+                    
+                    aux = l[j];
+                    l[j] = l[j+1];
+                    l[j+1] = aux;
+                    
+                    aux = p[j];
+                    p[j] = p[j+1];
+                    p[j+1] = aux;
+                    
+                    aux = c[j];
+                    c[j] = c[j+1];
+                    c[j+1] = aux;
+                }
+            }
+        }
+        
+        out[0] = id; out[1] = l; out[2] = p;
+        
+        return out;
+    }
+    
+    // Por la división del nº de peticiones y la longitud de los ficheros (Decreciente)
+    private static int[][] ordenaCintaDivisionDecr(int[] id, int[] l, int[] p){
+        int[][] out = {new int[id.length], new int[l.length], new int[p.length]};
+        int[] c = new int[id.length];
+        int i, j;
+        
+        //obtenemos el producto para la ordenacion respecto a él
+        for (i = 0; i<c.length; i++)    c[i] = p[i]/l[i];
+        
+        int aux;
+        boolean ord = false;
+        
+        //ordenamos por burbuja respecto al array de cocientes c
+        i = 0;
+        while ((i < id.length) && (!ord)){
+            i = i+1;
+            ord = true;
+            
+            for (j = 0; j < (id.length-i); j++){
+                if(c[j] < c[j+1]){//------------------ < para mayor a menor
+                    ord = false;
+                    aux = id[j];
+                    id[j] = id[j+1];
+                    id[j+1] = aux;
+                    
+                    aux = l[j];
+                    l[j] = l[j+1];
+                    l[j+1] = aux;
+                    
+                    aux = p[j];
+                    p[j] = p[j+1];
+                    p[j+1] = aux;
+                    
+                    aux = c[j];
+                    c[j] = c[j+1];
+                    c[j+1] = aux;
                 }
             }
         }
@@ -121,22 +419,81 @@ public class OrdenaCinta {
     
     public static void main(String[] args) {
         
-        int[][] aux = generaCintaStandart();
-        //int[][] aux = generaCinta(7, 1, 20, 1, 15);
+        //Los aux son los que se modificarán durante la ejecución
+        //int[][] aux = generaCintaStandart();
+        int[][] aux = generaCinta(7, 1, 20, 1, 15);
         int[] id = aux[0];
+        int[] idaux;
         int[] l = aux[1];
+        int[] laux;
         int[] p = aux[2];
+        int[] paux;
         
-        imprimeCinta(id, l, p);
+        imprimeCinta(id, l, p, "Sin ordenar");
         
-        aux = ordenaCinta(id, l, p);
-        id = aux[0];
-        l = aux[1];
-        p = aux[2];
+        //Longitud creciente
+        aux = ordenaCintaLongitudCrec(id, l, p);
+        idaux = aux[0];
+        laux = aux[1];
+        paux = aux[2];
         
-        imprimeCinta(id, l, p);
+        imprimeCinta(idaux, laux, paux, "Ordenado de menor a mayor por longitud de los ficheros");
         
-        //------------------- Comprobar!!!!!! --------------------------
+        //Longitud decreciente
+        aux = ordenaCintaLongitudDecr(id, l, p);
+        idaux = aux[0];
+        laux = aux[1];
+        paux = aux[2];
+        
+        imprimeCinta(idaux, laux, paux, "Ordenado de mayor a menor por longitud de los ficheros");
+        
+        //Peticiones decreciente
+        aux = ordenaCintaPeticionesDecr(id, l, p);
+        idaux = aux[0];
+        laux = aux[1];
+        paux = aux[2];
+        
+        imprimeCinta(idaux, laux, paux, "Ordenado de mayor a menor por nº de peticiones de los ficheros");
+        
+        //Peticiones Creciente
+        aux = ordenaCintaPeticionesCrec(id, l, p);
+        idaux = aux[0];
+        laux = aux[1];
+        paux = aux[2];
+        
+        imprimeCinta(idaux, laux, paux, "Ordenado de menor a mayor por nº de peticiones de los ficheros");
+        
+        //Producto de nº de peticiones y longitud Creciente
+        aux = ordenaCintaProductoCrec(id, l, p);
+        idaux = aux[0];
+        laux = aux[1];
+        paux = aux[2];
+        
+        imprimeCinta(idaux, laux, paux, "Ordenado de menor a mayor por Multiplicación de nº peticiones y longitud de los ficheros");
+        
+        //Producto de nº de peticiones y longitud Decreciente
+        aux = ordenaCintaProductoDecr(id, l, p);
+        idaux = aux[0];
+        laux = aux[1];
+        paux = aux[2];
+        
+        imprimeCinta(idaux, laux, paux, "Ordenado de mayor a menor por Multiplicación de nº peticiones y longitud de los ficheros");
+        
+        //Divisiones de nº de peticiones y longitud Creciente
+        aux = ordenaCintaDivisionCrec(id, l, p);
+        idaux = aux[0];
+        laux = aux[1];
+        paux = aux[2];
+        
+        imprimeCinta(idaux, laux, paux, "Ordenado de menor a mayor por la Division de nº peticiones y longitud de los ficheros");
+
+        //Divisiones de nº de peticiones y longitud Decreciente
+        aux = ordenaCintaDivisionDecr(id, l, p);
+        idaux = aux[0];
+        laux = aux[1];
+        paux = aux[2];
+        
+        imprimeCinta(idaux, laux, paux, "Ordenado de mayor a menor por la Division de nº peticiones y longitud de los ficheros");
     }
     
 }
